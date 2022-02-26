8798ea88917ed146efbc85c3bacbb8444d571e66
silinlin8 <1441308255@qq.com>
2022-02-26 12:36:13 +0800

    change
    change

--- trunk/proprietary/rt_wifi/rtpci/3.0.X.X/mt7610/ap/ap_apcli.c
+++ trunk/proprietary/rt_wifi/rtpci/3.0.X.X/mt7610/ap/ap_apcli.c
@@ -1081,10 +1081,10 @@ VOID ApCliIfMonitor(
  
 			if ((pMacEntry->AuthMode >= Ndis802_11AuthModeWPA)
 				&& (pMacEntry->PortSecured != WPA_802_1X_PORT_SECURED)
-				&& (RTMP_TIME_AFTER(pAd->Mlme.Now32 , (pApCliEntry->ApCliLinkUpTime + (30 * OS_HZ)))))
+				&& (RTMP_TIME_AFTER(pAd->Mlme.Now32 , (pApCliEntry->ApCliLinkUpTime + (40 * OS_HZ)))))
 				bForceBrocken = TRUE;
  
-			if (RTMP_TIME_AFTER(pAd->Mlme.Now32 , (pApCliEntry->ApCliRcvBeaconTime + (12 * OS_HZ))))
+			if (RTMP_TIME_AFTER(pAd->Mlme.Now32 , (pApCliEntry->ApCliRcvBeaconTime + (30 * OS_HZ))))
 			{
 				printk("ApCliIfMonitor: IF(%s%d) - no Beacon is received from Root-AP.\n", INF_APCLI_DEV_NAME, index);
 				bForceBrocken = TRUE;
