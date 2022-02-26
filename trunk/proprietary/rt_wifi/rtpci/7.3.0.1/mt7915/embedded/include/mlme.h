049d7430c7378a4c37e4447fa9630bda3e391919
silinlin8 <1441308255@qq.com>
2022-02-26 14:11:44 +0800

    Add files via upload

--- trunk/proprietary/rt_wifi/rtpci/7.3.0.1/mt7915/embedded/include/mlme.h
+++ trunk/proprietary/rt_wifi/rtpci/7.3.0.1/mt7915/embedded/include/mlme.h
@@ -95,9 +95,9 @@ extern UCHAR BROADCAST_ADDR[];
 #define CHN		5
 #define MAX_RD_REGION 6
 #if defined(CONFIG_MULTI_CHANNEL) || defined(DOT11Z_TDLS_SUPPORT)
-#define BEACON_LOST_TIME           (12 * OS_HZ)    /* 2048 msec = 2 sec */
+#define BEACON_LOST_TIME           (30 * OS_HZ)    /* 2048 msec = 2 sec */
 #else
-#define BEACON_LOST_TIME            (4 * OS_HZ)    /* 2048 msec = 2 sec */
+#define BEACON_LOST_TIME            (30 * OS_HZ)    /* 2048 msec = 2 sec */
 #endif /* defined(CONFIG_MULTI_CHANNEL) || defined(DOT11Z_TDLS_SUPPORT) */
 
 #define DLS_TIMEOUT                 1200      /* unit: msec */
