d9ce8327c1fdd7b819faf90119f0243d8151d769
silinlin8 <1441308255@qq.com>
2022-02-27 10:44:55 +0800

    Update mlme.c

--- trunk/proprietary/rt_wifi/rtsoc/4.1.X.X/mt7628/embedded/common/mlme.c
+++ trunk/proprietary/rt_wifi/rtsoc/4.1.X.X/mt7628/embedded/common/mlme.c
@@ -2462,7 +2462,7 @@ VOID STAMlmePeriodicExec(RTMP_ADAPTER *pAd)
 #endif /* WPA_SUPPLICANT_SUPPORT */
 
 		if (pAd->StaCfg.bSkipAutoScanConn &&
-			RTMP_TIME_BEFORE(pAd->Mlme.Now32, pAd->StaCfg.LastScanTime + (30 * OS_HZ)))
+			RTMP_TIME_BEFORE(pAd->Mlme.Now32, pAd->StaCfg.LastScanTime + (60 * OS_HZ)))
 			goto SKIP_AUTO_SCAN_CONN;
 		else
 			pAd->StaCfg.bSkipAutoScanConn = FALSE;
